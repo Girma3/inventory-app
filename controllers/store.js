ceea0e66caf6bfe61a34ab6be34e15f5910f5669
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2025-02-04 15:38:13 +0300

    feat: function to post form added

--- controllers/store.js
+++ controllers/store.js
@@ -1,4 +1,32 @@
+import { getAllCategories, insertCategory } from "../db/queries.js";
+import { body, validationResult } from "express-validator";
+let lengthErr = "must be between 3 and 20 characters";
+let validateUser = [
+  body("categoryName")
+    .trim()
+    .isEmpty()
+    .withMessage("Name can't be empty!")
+    .isLength({ min: 3, max: 20 })
+    .withMessage(`${lengthErr}`),
+];
+insertCategory("Strength Equipment");
 async function getHomePage(req, res) {
-  res.render("home", { title: "home" });
+  const allCategories = await getAllCategories();
+  // console.log(allCategories);
+  res.render("home", { title: "home", categories: allCategories });
 }
-export { getHomePage };
+async function getCategoryForm(req, res) {
+  let errors = validationResult(req);
+  console.log(errors);
+  if (!errors.isEmpty().length) {
+    res.status(400).render("category-form", {
+      title: "add Category",
+      errors: errors.array(),
+      value :req.body.categoryName
+    });
+  }
+  // const { categoryName } = req.body;
+  // console.log(categoryName);
+  // res.redirect("/");
+}
+export { getHomePage, getCategoryForm ,validateUser};
--- route/route.js
+++ route/route.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
-import { getHomePage } from "../controllers/store.js";
+import { getHomePage, getCategoryForm,validateUser } from "../controllers/store.js";
 const storeRouter = Router();
 storeRouter.get("/", getHomePage);
+storeRouter.get("/create/category", getCategoryForm);
+storeRouter.post("/create/category",validateUser, getCategoryForm);
 export { storeRouter };
