7a82917c04d9f8b178853045ed91bae44070dd45
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2025-02-11 18:55:17 +0300

    feat: function added to add new item using fong form

--- controllers/store.js
+++ controllers/store.js
@@ -4,15 +4,26 @@ import {
   getCategory,
   getTotalNumberOfItems,
   insertCategory,
+  deleteCategory,
+  deleteItem,
+  getItemById,
+  insertItem,
+  editCategory,
 } from "../db/queries.js";
 import { body, validationResult } from "express-validator";
-let lengthErr = "must be between 3 and 20 characters";
+let lengthErr = "must be between 3 and 40 characters";
 let validateUser = [
   body("categoryName")
     .trim()
-    .isEmpty()
+    .notEmpty()
     .withMessage("Name can't be empty!")
-    .isLength({ min: 3, max: 20 })
+    .isLength({ min: 3, max: 40 })
+    .withMessage(`${lengthErr}`),
+  body("categoryDescription")
+    .trim()
+    .notEmpty()
+    .withMessage("Description can't be empty!")
+    .isLength({ min: 3, max: 40 })
     .withMessage(`${lengthErr}`),
 ];
 insertCategory("Strength Equipment");
@@ -28,12 +39,14 @@ async function getHomePage(req, res) {
         return { ...category, itemCount };
       })
     );
-    console.log(totalItemCount);
-    console.log(categoriesWithItemCounts);
+    // console.log(totalItemCount);
+    // console.log(categoriesWithItemCounts);
     res.render("home", {
       title: "home",
       categories: allCategories,
       totalItems: totalItemCount,
+      errors: [],
+      category: null,
     });
   } catch (err) {
     console.log("can't get category data", err);
@@ -42,36 +55,139 @@ async function getHomePage(req, res) {
 }
 async function getCategoryForm(req, res) {
   let errors = validationResult(req);
-  console.log(errors);
-  if (!errors.isEmpty().length) {
+
+  console.log(req.body);
+  if (!errors.isEmpty()) {
+    console.log(errors);
     res.status(400).render("category-form", {
       title: "add Category",
       errors: errors.array(),
       value: req.body.categoryName,
     });
   }
-  // const { categoryName } = req.body;
-  // console.log(categoryName);
-  // res.redirect("/");
+  const { categoryName, categoryDescription } = req.body;
+  try {
+    await insertCategory(categoryName, categoryDescription);
+  } catch (err) {
+    console.log(err, "can't create category");
+  } finally {
+    res.redirect("/");
+  }
 }
 async function getDetailPage(req, res) {
+  let errors = validationResult(req);
+
   const categoryId = req.params.id;
+
+  console.log(categoryId);
   try {
     const selectedCategory = await getCategory(categoryId);
-    if (!selectedCategory.length) {
+    if (!selectedCategory) {
       return res.status(404).send("Category not found");
     }
     const selectedCategoryItems = await getAllItems(categoryId);
     const categoryName = selectedCategory[0].category_name;
+    // console.log(selectedCategory);
+
     res.render("detail-page", {
       title: categoryName,
       categoryName: categoryName,
       categoryId: categoryId,
       items: selectedCategoryItems,
+      category: selectedCategory[0],
+      errors: errors.array(),
+      item: null,
     });
   } catch (err) {
     console.error("Error getting detail page", err);
     res.status(500).send("Server Error");
   }
 }
-export { getHomePage, getCategoryForm, validateUser, getDetailPage };
+
+async function handleDeleteCategory(categoryId) {
+  console.log(categoryId);
+  try {
+    await deleteCategory(categoryId);
+    res.redirect("/");
+  } catch (err) {
+    console.log("can't Delete category", err);
+    res.status(500).send("Server Error");
+  }
+}
+async function handleDeleteItem(req, res) {
+  const itemId = req.params.id;
+  console.log(itemId);
+  try {
+    await deleteItem(itemId);
+  } catch (err) {
+    console.log(err);
+  }
+}
+async function handleItemEdit(req, res) {
+  let errors = validationResult(req);
+
+  //res.status(200).json(selectedItem[0]);
+
+  try {
+    const itemId = req.params.id;
+    let categoryId = 1;
+
+    const selectedItem = await getItemById(itemId);
+
+    const selectedCategory = await getCategory(
+      selectedItem[0].item_category_id
+    );
+    categoryId = selectedCategory[0].category_id;
+
+    // const selectedCategoryItems = await getAllItems(categoryId);
+    // const categoryName = selectedCategory[0].category_name;
+    res.status(200).json(selectedItem[0]);
+  } catch (err) {
+    console.log(err);
+  }
+}
+async function handleAddItem(req, res, categoryId) {
+  console.log(categoryId);
+  console.log(req.body);
+  if (req.body) {
+    const { itemName, itemDescription, itemPrice, itemImage } = req.body;
+    console.log(itemName);
+    try {
+      await insertItem(
+        itemName,
+        itemDescription,
+        itemPrice,
+        itemImage,
+        categoryId
+      );
+
+      res.redirect(`/category/${categoryId}`);
+    } catch (err) {
+      console.log(err, "can't add new item");
+    }
+  }
+}
+async function handleUpdateCategory(req, res, category) {
+  console.log(category);
+  console.log(req.body);
+  const { categoryName, categoryDescription } = req.body;
+  if (!req.body) return;
+  try {
+    await editCategory(category, categoryName, categoryDescription);
+  } catch (err) {
+    console.log(err, "can't update category");
+  } finally {
+    res.redirect(`/category/${category}`);
+  }
+}
+export {
+  getHomePage,
+  getCategoryForm,
+  validateUser,
+  getDetailPage,
+  handleDeleteCategory,
+  handleItemEdit,
+  handleDeleteItem,
+  handleAddItem,
+  handleUpdateCategory,
+};
