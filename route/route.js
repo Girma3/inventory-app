5c23ebafb2ae49b86c48b89494b760a6201417c8
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2025-02-18 17:14:43 +0300

    feat: route added to move item

--- route/route.js
+++ route/route.js
@@ -1,7 +1,51 @@
 import { Router } from "express";
-import { getHomePage, getCategoryForm,validateUser } from "../controllers/store.js";
+import {
+  getHomePage,
+  getCategoryForm,
+  validateCategory,
+  getDetailPage,
+  handleDeleteCategory,
+  handleItemEdit,
+  handleDeleteItem,
+  handleAddItem,
+  handleUpdateCategory,
+  handleItemJson,
+  validateItem,
+  handleCategoryJson,
+  handleItemMove,
+} from "../controllers/store.js";
 const storeRouter = Router();
+
 storeRouter.get("/", getHomePage);
+//create category form
+
 storeRouter.get("/create/category", getCategoryForm);
-storeRouter.post("/create/category",validateUser, getCategoryForm);
+storeRouter.post("/create/category", validateCategory, getCategoryForm);
+
+storeRouter.get("/category/:id", getDetailPage);
+storeRouter.post(
+  "/update/category/:id",
+  validateCategory,
+  handleUpdateCategory
+);
+storeRouter.get("/category/json/:id", handleCategoryJson);
+
+storeRouter.delete("/delete/category/:id", handleDeleteCategory);
+
+//storeRouter.get("/new/item/:id", handleAddItem);
+
+storeRouter.get("/item/json/:id", handleItemJson);
+storeRouter.post("/item/new/:id", validateItem, handleAddItem);
+storeRouter.post("/update/item/:id", validateItem, handleItemEdit);
+storeRouter.put("/move/item/:id", handleItemMove);
+storeRouter.delete("/delete/item/:id", handleDeleteItem);
+//update category
+// storeRouter.get("/category/update/:id", (req, res) => {
+//   const category = req.params.id;
+
+//   handleUpdateCategory(req, res, category);
+// });
+
+//get category as json
+
 export { storeRouter };
